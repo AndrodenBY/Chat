a26f9fc2f7f8cc62f2c247818bcc280d9edd9ff1
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2026-07-23 16:04:02 +0200

    refactor: api documentation

--- src/Chat.Authorization/Chat.Api/Program.cs
+++ src/Chat.Authorization/Chat.Api/Program.cs
@@ -1,12 +1,40 @@
-using Chat.Authorization.Extensions;
 using Chat.Infrastructure;
+using Chat.Infrastructure.Options;
+using Chat.Infrastructure.Options.Keycloak;
+using Microsoft.Extensions.Options;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.AddUserSecrets<Program>();
+builder.Configuration.AddEnvironmentVariables();
 builder.Services.AddOpenApi();
-builder.Services.AddScalarDocumentation();
 builder.Services.AddInfrastructure();
+builder.Services.AddOptions<ScalarOptions>()
+    .Configure<IOptions<IdentityProviderOptions>, IOptions<KeycloakOptions>>(
+        (options, identityProviderConfig, keycloakConfig) =>
+        {
+            var identityProvider = identityProviderConfig.Value;
+            var keycloakOptions = keycloakConfig.Value;
+            
+            const string securitySchemeName = "OAuth2";
+
+            options
+                .WithTitle("Chat API Reference")
+                .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient)
+                .AddPreferredSecuritySchemes(securitySchemeName)
+                .AddAuthorizationCodeFlow(securitySchemeName, flow =>
+                {
+                    flow.ClientId = keycloakOptions.UserClient.ClientId;
+                    flow.ClientSecret = keycloakOptions.UserClient.ClientSecret;
+                    
+                    flow.SelectedScopes = identityProvider.SelectedScopes;
+                    
+                    flow.Pkce = Enum.TryParse<Pkce>(keycloakOptions.Pkce, true, out var parsedPkce)
+                        ? parsedPkce
+                        : Pkce.No;
+                });
+        });
 
 var app = builder.Build();
 
