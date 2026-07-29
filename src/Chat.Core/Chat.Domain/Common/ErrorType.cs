824d89f4f13c9c92da6be813b238bb9ac015ff6e
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2026-07-29 23:03:03 +0200

    feat(core): add Result pattern

---
+++ src/Chat.Core/Chat.Domain/Common/ErrorType.cs
@@ -0,0 +1,9 @@
+namespace Chat.Domain.Common;
+
+public enum ErrorType
+{
+    Validation,
+    NotFound,
+    Failure,
+    Conflict
+}
---
+++ src/Chat.Core/Chat.Domain/Common/Result.cs
@@ -0,0 +1,27 @@
+namespace Chat.Domain.Common;
+
+public readonly struct Result
+{
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+    public IReadOnlyList<Error> Errors  { get; }
+    
+    public Error PrimaryError => Errors.Count > 0 
+        ? Errors[0] 
+        : Error.None;
+
+    private Result(bool isSuccess, IReadOnlyList<Error> errors)
+    {
+        IsSuccess = isSuccess;
+        Errors = errors;
+    }
+    
+    public static Result Success => new(true, []);
+    public static Result Failure(Error error) => new(false, [error]);
+    public static Result Failure(IReadOnlyList<Error> errors) => new(false, errors);
+    
+    public static implicit operator Result(Error error) => Failure(error);
+    public static implicit operator Result(List<Error> errors) => Failure(errors);
+    
+    
+}
