34af8d8e322d32a8e76c3aa804fba0ee2131e896
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2026-07-30 12:11:22 +0200

    feat(core): add pagination

---
+++ src/Chat.Core/Chat.Domain/Common/Pagination/PaginationExtensions.cs
@@ -0,0 +1,27 @@
+namespace Chat.Domain.Common.Pagination;
+
+public static class PaginationExtensions
+{
+    public static PaginatedList<TItem> ToPagedList<TItem>(
+        this IEnumerable<TItem> source,
+        int pageNumber, 
+        int pageSize, 
+        int totalCount)
+    {
+        var appliedPageSize = pageSize > 0 
+            ? pageSize 
+            : PaginationConstants.DefaultPageSize;
+        
+        var pageCount = (int)Math.Ceiling(totalCount / (double)appliedPageSize);
+        
+        var appliedPageNumber = totalCount == 0 
+            ? 0 
+            : Math.Clamp(pageNumber, 1, Math.Max(1, pageCount));
+
+        return new PaginatedList<TItem>(
+            source.ToList(), 
+            appliedPageNumber, 
+            appliedPageSize, 
+            totalCount);
+    }
+}
