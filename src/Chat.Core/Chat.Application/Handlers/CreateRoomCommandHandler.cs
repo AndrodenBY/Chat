e4af15591e288c9b105bfd7b541eda779a3db4c3
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2026-08-03 11:23:11 +0200

    feat(core): implement CreateRoomCommandHandler

---
+++ src/Chat.Core/Chat.Application/Handlers/CreateRoomCommandHandler.cs
@@ -0,0 +1,33 @@
+using Chat.Application.Commands;
+using Chat.Application.Contracts;
+using Chat.Application.DTOs;
+using Chat.Domain.Common.Result;
+using Chat.Domain.Contracts;
+using Chat.Domain.Entities;
+using Chat.Domain.ValueObjects;
+using DispatchR.Abstractions.Send;
+
+namespace Chat.Application.Handlers;
+
+public class CreateRoomCommandHandler(
+    IRepository<ChatRoom, RoomId> roomRepository,
+    ISnowflakeIdGenerator idGenerator) 
+    : IRequestHandler<CreateRoomCommand, ValueTask<Result<ChatRoomDto>>>
+{
+    public async ValueTask<Result<ChatRoomDto>> Handle(CreateRoomCommand request, CancellationToken cancellationToken)
+    {
+        var roomId = RoomId.From(idGenerator.NextId());
+        var roomResult = ChatRoom.Create(roomId, request.Name, request.Description);
+        if (roomResult.IsFailure)
+        {
+            return roomResult.PrimaryError;
+        }
+        
+        var roomInstance = await roomRepository.Add(roomResult, cancellationToken);
+        
+        return new ChatRoomDto(
+            (string)roomInstance.Name, 
+            (string)roomInstance.Description!
+        );
+    }
+}
