3a4d8336fd8245b22f8387709fa406adfc41c26d
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2026-08-03 11:21:30 +0200

    refactor(core.domain): add implicit operator to Result<T>
    # Conflicts:
    #	src/Chat.Core/Chat.Domain/Common/Result/ResultT.cs

---
+++ src/Chat.Core/Chat.Domain/Common/Result/ResultT.cs
@@ -0,0 +1,34 @@
+namespace Chat.Domain.Common.Result;
+
+public readonly struct Result<TValue>
+{
+    private readonly TValue? _value;
+    
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
+    public IReadOnlyList<Error> Errors { get; }
+    
+    public Error PrimaryError => Errors.Count > 0 
+        ? Errors[0]
+        : Error.None;
+
+    public TValue Value => IsSuccess 
+        ? _value! 
+        : throw new InvalidOperationException("Cannot access the value of a failed result.");
+
+    private Result(TValue? value, bool isSuccess, IReadOnlyList<Error> errors)
+    {
+        IsSuccess = isSuccess;
+        _value = value;
+        Errors = errors;
+    }
+
+    public static Result<TValue> Success(TValue value) => new(value, true, []);
+    public static Result<TValue> Failure(Error error) => new(default, false, [error]);
+    public static Result<TValue> Failure(IReadOnlyList<Error> errors) => new(default, false, errors);
+    
+    public static implicit operator Result<TValue>(TValue value) => Success(value);
+    public static implicit operator Result<TValue>(Error error) => Failure(error);
+    public static implicit operator Result<TValue>(List<Error> errors) => Failure(errors);
+    public static implicit operator TValue(Result<TValue> result) => result.Value;
+}
