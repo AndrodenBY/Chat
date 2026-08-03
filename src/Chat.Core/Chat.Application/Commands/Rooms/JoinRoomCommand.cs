2c91b810124583593cbf87e4d5c57aa8506c10ed
AndrodenBY <56772046+AndrodenBY@users.noreply.github.com>
2026-08-03 14:28:51 +0200

    refactor(core.application): modify command arguments

--- src/Chat.Core/Chat.Application/Commands/Rooms/JoinRoomCommand.cs
+++ src/Chat.Core/Chat.Application/Commands/Rooms/JoinRoomCommand.cs
@@ -4,5 +4,5 @@ using DispatchR.Abstractions.Send;
 
 namespace Chat.Application.Commands.Rooms;
 
-public record JoinRoomCommand(string ConnectionId, UserConnection Connection) 
-    : IRequest<JoinRoomCommand, ValueTask<Result<string>>>;
+public record JoinRoomCommand(string ConnectionId, long RoomId, string UserId, string Username)
+    : IRequest<JoinRoomCommand, ValueTask<Result<bool>>>;
